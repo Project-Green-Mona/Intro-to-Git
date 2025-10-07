4f8f61971123802a8fe91c91ed56a44f05b0bb84
habramalcantar <117396638+habramalcantar@users.noreply.github.com>
2025-10-07 15:17:38 -0400

    Initial Commit

---
+++ IntroToGit/IntroToGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToGitApp.swift
+//  IntroToGit
+//
+//  Created by Habram Alcantar on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct IntroToGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
