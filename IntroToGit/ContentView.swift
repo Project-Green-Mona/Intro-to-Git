e0e5878b492c2c12824f6a76936e844155edc105
habramalcantar <117396638+habramalcantar@users.noreply.github.com>
2025-10-07 17:20:29 -0400

    feat: MTT swipe deck

--- IntroToGit/ContentView.swift
+++ IntroToGit/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
+struct TeamMember : Identifiable {
+    let id = UUID()
+    let name : String
+    let role : String
+    let imageName : String
+}
+
+let teamMembers = [
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            
+            Text("Mona: Meet the Team")
+                .font(.largeTitle.bold())
+                .padding([.top, .leading])
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            TabView {
+                ForEach(teamMembers) { member in
+                    VStack {
+                        Image(member.imageName)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 250, height: 250)
+                            .clipShape(Circle())
+                            .shadow(radius: 5)
+                        
+                        Text(member.name)
+                            .font(.headline)
+                            .padding(.top, 10)
+                        
+                        Text(member.role)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(20)
+                    .shadow(color: .gray.opacity(0.3), radius: 5, x: 0, y: 3)
+                    .padding(.vertical, 40)
+                    .padding(.horizontal, 20)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .frame(height: 500)
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
